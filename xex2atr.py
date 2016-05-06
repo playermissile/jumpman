b7964ece6db1c69aeee8ddcfa04d15465da38f8d
Rob McMullen <feedback@playermissile.com>
2016-05-05 23:13:25 -0700

    Using new utility functions from atrcopy to generate xexboot and atr headers

--- xex2atr.py
+++ xex2atr.py
@@ -2,28 +2,16 @@
 
 import numpy as np
 
-from atrcopy import SegmentData, BootDiskImage
+from atrcopy import SegmentData, BootDiskImage, add_xexboot_header, add_atr_header
 
 def make_bootable_atr(xex_path, header_path, atr_path):
     xex = np.fromfile(xex_path, dtype=np.uint8)
-    xex_size = np.alen(xex)
     bootcode = np.fromfile(header_path, dtype=np.uint8)
-    v = bootcode[9:11].view(dtype="<u2")
-    v[0] = xex_size
-    size = np.alen(bootcode)
-    bootsectors = np.zeros([384], dtype=np.uint8)
-    bootsectors[0:size] = bootcode
-
-    _, remainder = divmod(xex_size, 128)
-    if remainder > 0:
-        xex = np.append(xex, np.zeros([128-remainder], dtype=np.uint8))
-
-    data = np.append(bootsectors, xex)
-    rawdata = SegmentData(data)
+    bootdata = add_xexboot_header(xex, bootcode)
+    atrdata = add_atr_header(bootdata)
+    rawdata = SegmentData(atrdata)
     atr = BootDiskImage(rawdata, atr_path)
-    atr.rebuild_header()
     print atr.header
-    print bootcode[0:16]
     atr.save()
 
 if __name__ == "__main__":
