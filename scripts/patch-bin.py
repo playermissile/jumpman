10d7ff48906b8fcb55a65a89bcb2a15267ef8043
Rob McMullen <feedback@playermissile.com>
2026-05-18 18:34:25 -0700

    Added offset and byte lists to patch-bin.py

--- scripts/patch-bin.py
+++ scripts/patch-bin.py
@@ -50,21 +50,24 @@ def add_bytes(data, num):
 
 def iter_patch(patch_path, names):
     org = None
+    offset = 0
     data = []
     with open(patch_path) as fh:
         for line in fh.readlines():
             line = line.lstrip()
             if not line or line.startswith(";"):
                 continue
-            # tokens are comma seperated values
-            cmd, tokens = line.split(None, 1)
-            tokens = [x.strip() for x in tokens.split(',')]
+            # tokens are comma seperated values or space separated values
+            cmd, args = line.split(None, 1)
+            tokens = [x.strip() for x in args.split(',')]
             cmd = cmd.lower()
             if cmd == ".org":
                 if org and data:
-                    yield org, org + len(data), data
+                    yield offset + org, offset + org + len(data), data
                     data = []
                 org = text_to_int(tokens[0])
+            elif cmd == ".offset":
+                offset = text_to_int(tokens[0])
             elif cmd == ".byte":
                 values = []
                 for v in tokens:
@@ -81,16 +84,27 @@ def iter_patch(patch_path, names):
                     else:
                         add_bytes(values, text_to_int(v))
                 data.extend(values)
+            elif cmd.endswith(":"):
+                values = []
+                int = text_to_int(cmd[0:-1], 'hex')
+                tokens = [x.strip() for x in args.split()]
+                for v in tokens:
+                    add_bytes(values, text_to_int(v, "hex"))
+                data.extend(values)
+
         if org and data:
-            yield org, org + len(data), data
+            yield offset + org, offset + org + len(data), data
 
 
 
 
 def patch_image(src_path, patch_path, list_path, dest_path):
     src = np.fromfile(src_path, dtype=np.uint8)
-    names = nm(list_path)
-    print(names)
+    if list_path:
+        names = nm(list_path)
+        print(names)
+    else:
+        names = {}
     for start, end, data in iter_patch(patch_path, names):
         print(start, end, data)
         src[start:end] = data
@@ -98,9 +112,15 @@ def patch_image(src_path, patch_path, list_path, dest_path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Insert file into another file")
+    parser.add_argument('src_file')           # Jumpman Level Tester ATR disk image
+    parser.add_argument('patch_file', type=str)           # XEX of assembled level source code
     parser.add_argument("-v", "--verbose", default=0, action="count")
     parser.add_argument("-d", "--debug", action="store_true", default=False, help="debug the currently under-development parser")
     parser.add_argument("-o", "--output", default="", help="output file")
     options, extra_args = parser.parse_known_args()
+    if extra_args:
+        list_file = extra_args[0]
+    else:
+        list_file = ""
 
-    patch_image(extra_args[0], extra_args[1], extra_args[2], options.output)
+    patch_image(options.src_file, options.patch_file, list_file, options.output)
