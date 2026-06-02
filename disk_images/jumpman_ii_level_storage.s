b3e60d69d0f765bd70b526f8b1f7a6b737a83f72
Rob McMullen <feedback@playermissile.com>
2026-06-02 16:17:23 -0700

    Added lz4 decompression and example file
    * removed old binary levelbuilder2.xex since this is built from source

--- disk_images/jumpman_ii_level_storage.s
+++ disk_images/jumpman_ii_level_storage.s
@@ -48,13 +48,18 @@ unpack_level_v2:
         lda $2861
         sta $e5
 
-        lda $2800
-;        bmi decompress_level
+        lda $2866
+        beq @1
+        cmp #1
+        bne fail
+        jsr unpack_lz4
+        clc
+        bcc handle_block_b
+@1:     jsr copy_uncompressed
 
-uncompressed_level:
-        jsr copyblock
-
-        lda $2864       ; dest in $(e2): normally $a800
+handle_block_b:
+        ; e0 will be pointing to first byte of Block B raw data
+        lda $2864        ; dest in $(e2): typically $a800
         sta $e2
         lda $2865
         sta $e3
@@ -62,7 +67,20 @@ uncompressed_level:
         sta $e4
         lda $2863
         sta $e5
-copyblock:
+
+        lda $2867
+        beq @1
+        cmp #1
+        bne fail
+        jsr unpack_lz4
+        rts
+@1:     jsr copy_uncompressed
+        rts
+
+fail:
+        jmp $455c       ; unsupported compression algorithm
+
+copy_uncompressed:
         ldy #0
         ldx $e5         ; number of pages
         beq @partial
@@ -90,3 +108,125 @@ copyblock:
         bcc @exit
         inc $e1
 @exit:  rts
+
+
+
+;LZ4 data decompressor for Apple II
+;Peter Ferrie (peter.ferrie@gmail.com)
+;
+;from http://pferrie.host22.com/misc/appleii.htm
+;Converted to ATasm by Rob McMullen
+;
+; raw compressed data in $e0
+; destination address in $e2
+; count in $e4, which is then reused as a temp variable
+src = $e0
+dst = $e2
+count = $e4
+end = $e6
+delta = $e8
+
+unpack_lz4:
+        ldy	#0
+        clc
+        lda src
+        adc count
+        sta end
+        lda src+1
+        adc count+1
+        sta end+1
+
+parsetoken:
+    jsr	getsrc
+    pha
+    lsr
+    lsr
+    lsr
+    lsr
+    beq	copymatches
+    jsr	buildcount
+    tax
+    jsr	docopy
+    lda	src
+    cmp	end
+    lda	src+1
+    sbc	end+1
+    bcs	done
+
+copymatches:
+    jsr	getsrc
+    sta	delta
+    jsr	getsrc
+    sta	delta+1
+    pla
+    and	#$0f
+    jsr	buildcount
+    clc
+    adc	#4
+    tax
+    bcc	@1
+    inc	count+1
+@1:	lda	src+1
+    pha
+    lda	src
+    pha
+    sec
+    lda	dst
+    sbc	delta
+    sta	src
+    lda	dst+1
+    sbc	delta+1
+    sta	src+1
+    jsr	docopy
+    pla
+    sta	src
+    pla
+    sta	src+1
+    jmp	parsetoken
+
+done:
+    pla
+    rts
+
+docopy:
+    jsr	getput
+    dex
+    bne	docopy
+    dec	count+1
+    bne	docopy
+    rts
+
+buildcount:
+    ldx	#1
+    stx	count+1
+    cmp	#$0f
+    bne	@3
+@1:	sta	count
+    jsr	getsrc
+    tax
+    clc
+    adc	count
+    bcc	@2
+    inc	count+1
+@2:	inx
+    beq	@1
+@3:	rts
+
+getput:
+    jsr	getsrc
+
+putdst:
+    sta (dst), y
+    inc	dst
+    beq @1
+    rts
+@1:	inc	dst+1
+    rts
+
+getsrc:
+    lda (src), y
+    inc	src
+    beq	@1
+    rts
+@1:	inc	src+1
+    rts
